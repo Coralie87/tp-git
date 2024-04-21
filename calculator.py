059102f64de90def4524d1e1f27b4a8d71bb2986
Coralie87 <coralierouveyrol@gmail.com>
2024-04-21 18:23:14 +0200

    etape 3.1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,18 @@
 def addition(a, b):
     result = a + b
     return result
+
+
+
+def subtraction(a, b):
+    result = a - b
+    return result
+
+
+
+
+def division(a, b):
+    if b == 0:
+        raise ValueError("Division by zero is not allowed")
+    result = a / b
+    return result
